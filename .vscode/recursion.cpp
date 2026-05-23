0e61e2731c91ddf54a78c164986fa8e27961c811
komal <gylkomal942@gmail.com>
2026-05-23 17:12:25 +0530

    cpp practice questions

--- .vscode/recursion.cpp
+++ .vscode/recursion.cpp
@@ -350,4 +350,187 @@
 //     return 0 ; 
 // }
 
-//
+//check recursion 
+// #include<iostream>
+// using namespace std ;
+// bool checkpal(string str , int start , int end ){
+//     //base condition 
+//     if(start >= end){
+//         return 1 ; 
+//     }
+//     if(str[start] != str[end] ){
+//         return 0 ; 
+//     }
+//     else{
+//         return checkpal(str,start+1 , end-1) ; ; 
+//     }
+    
+// }
+// int main(){
+//     string str = "namat" ; 
+
+//     cout<<checkpal(str,0,4) ; 
+//     return 0 ; 
+// }
+
+//count vowels 
+// #include<iostream>
+// using namespace std ;
+// int count(string str , int start , int end ){
+//     if(start>=end) {
+//         return 0; 
+//     }
+//     if(str[start] == 'a'|| str[start] == 'e'||str[start] =='i'||str[start] =='o'||str[start] =='u'){
+//         return 1 + count(str , start+1 , end-1) ;
+//     }
+//     else{
+//         return count(str , start+1 , end-1) ;
+//     }
+    
+// }
+// int main(){
+//      string str = "komal" ; 
+//      cout<<count(str , 0  ,4 ) ; 
+//      return 0 ; 
+// }
+
+
+// #include<iostream>
+// using namespace std ;
+// int count(string str , int index ){
+//     if(index == -1) {
+//         return 0; 
+//     }
+//     if(str[index] == 'a'|| str[index] == 'e'||str[index] =='i'||str[index] =='o'||str[index] =='u'){
+//         return 1 + count(str , index-1) ;
+//     }
+//     else{
+//        return  count(str , index-1 ) ;
+//     }
+    
+// }
+// int main(){
+//      string str = "koeal" ; 
+//      cout<<count(str , 4 ) ; 
+//      return 0 ; 
+// }
+
+//reverse a string 
+// #include<iostream>
+// using namespace std ;
+// void reverse(string &str , int start , int end) {
+//     if(start >= end){
+//         return ; 
+//     }
+//     char c = str[start] ;
+//     str[start] = str[end] ;
+//     str[end] = c ;  
+// reverse(str , start+1 , end-1 ) ; 
+// }  
+// int main(){
+//      string str = "komal" ; 
+//      reverse(str , 0 , 4 ) ; 
+//      cout<<str<<endl;
+//      return 0 ;   
+// }
+
+//lower case to upper 
+// #include<iostream>
+// using namespace std ;
+// void lowertoupper(string &str , int index){
+//     if(index == -1){
+//         return ; 
+//     }
+//     str[index] = 'A'+str[index]-'a';
+//     lowertoupper(str , index-1) ; 
+// }
+// int main(){
+//      string str = " komal" ; 
+//      lowertoupper(str ,4);
+//      cout<<str ; 
+//      return 0 ; 
+// }
+
+//linear search 
+// #include<iostream>
+// using namespace std ; 
+// bool linear(int arr[] , int index , int x , int n) {
+//     if(index== n){
+//         return 0 ; 
+//     }
+//     if(arr[index] == x){
+//         return 1 ; 
+//     }
+//     else{
+//         return linear(arr , index+1 , x , n) ; 
+//     }
+// }
+// int main(){
+//      int arr[] = { 2,4,6,8,3,4,5,9} ; 
+//      int x = 6 ; 
+//      cout<<linear(arr, 0 , x , 7) ;
+//      return 0 ;
+// }
+
+//linear search with index
+// #include<iostream>
+// using namespace std ; 
+// int linear(int arr[] , int index , int x , int n) {
+//     if(index== n){
+//         return -1 ; 
+//     }
+//     if(arr[index] == x){
+//         return index ; 
+//     }
+//     else{
+//         return linear(arr , index+1 , x , n) ; 
+//     }
+// }
+// int main(){
+//      int arr[] = { 2,4,6,8,3,4,5,9} ; 
+//      int x = 6 ; 
+//      cout<<linear(arr, 0 , x , 7) ;
+//      return 0 ;
+// }
+
+//binary search 
+// #include<iostream>
+// using namespace std ; 
+// int binary( int arr[] , int start , int end , int x) {
+//     int mid = start + ((end-start)/2) ; 
+//     if(start>end) {
+//         return -1 ; 
+//     }
+//     if(arr[mid] == x) {
+//         return mid ; 
+//     }
+//     else if(arr[mid] < x ){
+//         return binary(arr , mid+1 , end , x) ;
+//     }
+//     else{
+//         return binary(arr,start,mid-1 , x) ; 
+//     }
+// }
+// int main(){
+//     int arr[] = { 3,8,11,15,20,22}  ;// non-decrning order 
+//     int x = 20 ; 
+//     cout<<binary( arr , 0 , 5 , x) ; 
+//     return 0 ; 
+// }
+
+// time and space complexity 
+// TOTAL TIME TAKEN BY AN ALGORITHM TO RUN OR A FUNCTION OF ITS INPUT SIZE
+
+// time complexity => print(n) == o(n) == 1 + print(n-1) => 1 + print(n-2).......=>1 + print(1)== 1+1 == 2 => print(1) == 1 
+
+// T(n) = 1 + T(n-1)  == k + T(n-k)
+
+//space complxity => print(n) = o(n) 
+//space complxity => reference variable => o{n^2)
+
+// time complexity of binary search  => o( log(base 2)n pppp
+//space complexity of binary search => o(log(base 2)n
+
+//time complexity of fibonacci series => o(2^n) 
+//space complexity of fibonacci series => o(n)       hint = sbse bda tree (check stack ) 
+
--- .vscode/tempCodeRunnerFile.cpp
+++ .vscode/tempCodeRunnerFile.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 using namespace std ; 
-int min(int arr[] , int index ,int n) {
-    if( index == n-1) {
-        return arr[index] ; 
+int binary( int arr[] , int start , int end , int x) {
+    int mid = start + ((end-start)/2) ; 
+    if(start>end) {
+        return -1 ; 
     }
-    return max(arr[index] , min(arr , index+1,n )) ; 
+    if(arr[mid] == x) {
+        return mid ; 
+    }
+    else if(arr[mid] < x ){
+        return binary(arr , mid+1 , end , x) ;
+    }
+    else{
+        return binary(arr,start,mid-1 , x) ; 
+    }
 }
 int main(){
-    int arr[] = { 3,4,21,4,5} ; 
-    cout<<min(arr , 0 , 5 ) ; 
+    int arr[] = { 3,8,11,15,20,22}  ;// non-decrning order 
+    int x = 20 ; 
+    cout<<binary( arr , 0 , 5 , x) ; 
     return 0 ; 
 }
